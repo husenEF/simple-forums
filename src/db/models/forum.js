bb41dc8b8558b1efb7d637f2b25ba71593db9090
harlita <harlitad@gmail.com>
2021-05-18 10:07:09 +0700

    set paranoid

--- src/db/models/forum.js
+++ src/db/models/forum.js
@@ -22,6 +22,7 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'Forum',
     underscored: true,
+    paranoid: true
   });
   return Forum;
 };
--- src/db/models/user.js
+++ src/db/models/user.js
@@ -23,6 +23,7 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'User',
     underscored: true,
+    paranoid: true
   });
   return User;
 };
