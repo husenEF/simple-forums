2d5f0dc9a83e57974421e36e1a75e6b2d8da7042
harlita <harlitad@gmail.com>
2021-05-18 13:18:35 +0700

    add middleware to auth route

--- src/routes/v1/auth.js
+++ src/routes/v1/auth.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-
 const authController = require("../../controllers/v1/authController");
+const SchemaValidation = require("../../helpers/schemaValidation");
+const validate = require("../../middlewares/validation");
 
 /* GET home page. */
-router.post("/login", authController.login);
+router.post("/login", validate(SchemaValidation.login()),authController.login);
 router.post("/register", authController.register);
-router.get("/test", authController.test);
+// router.get("/test", authController.test);
 
 module.exports = router;
