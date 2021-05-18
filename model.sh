68f49f2bdd350597d547619f15404ae2f695b5ae
harlita <harlitad@gmail.com>
2021-05-18 11:15:58 +0700

    create script shell migration

--- model.sh
+++ model.sh
@@ -1,5 +1,14 @@
-sequelize-cli model:generate --name User \
+npx sequelize-cli model:generate --name User \
 --attributes username:string,email:string,status:enum,role:enum,password:text --underscored
 
-sequelize-cli model:generate --name Forum \
+npx sequelize-cli model:generate --name Forum \
 --attributes title:string,description:text,thumbnail:text,status:enum --underscored
+
+npx sequelize-cli model:generate --name Thread \
+--attributes forum_id:integer,user_id:integer,title:text,description:text,status:enum --underscored
+
+npx sequelize-cli model:generate --name Reply  \
+--attributes user_id:integer,thread_id:integer,body:text,status:enum --underscored
+
+npx sequelize-cli model:generate --name ForumModerator  \
+--attributes user_id:integer,forum_id:integer --underscored
