71f8832053b603d3b3105cdba0e5639ca7999bad
harlita <harlitad@gmail.com>
2021-05-18 13:20:10 +0700

    update auth

--- src/controllers/v1/authController.js
+++ src/controllers/v1/authController.js
@@ -1,11 +1,10 @@
+const db = require('./../../db/models')
 class authController {
-  static async login(req, res, next) {}
-  static async register(req, res, next) {}
-  static test(req, res, next) {
-    res.json({
-      msg: "auth test",
-    });
+  static async login(req, res, next) {
+    const {username, password} = req.body
+    const user = db.User.findOne()
   }
+  static async register(req, res, next) {}
 }
 
 module.exports = authController;
