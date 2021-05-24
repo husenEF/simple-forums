963a1890b2db2956a06accc246d17b17ded146e9
Husen Efendi <husenef@gmail.com>
2021-05-24 16:24:13 +0700

    update

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const v1 = require("./src/routes/v1");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(express.par)
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(errorHandler)
