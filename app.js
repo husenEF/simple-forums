d9253205a4d1e1ac1c0d912a82a1eb840e2fbaef
harlita <harlitad@gmail.com>
2021-05-28 12:06:03 +0700

    change root router

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(errorHandler)
 
 // app.use("/api/",)
-router.use("/", indexRouter);
+app.use("/", indexRouter);
 router.use("/users", usersRouter);
 app.use("/api/v1", v1);
 
